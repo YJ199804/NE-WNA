2b4917661c1158fdb495b5a21b6bdb9fa472a405
YJ199804 <67402412+YJ199804@users.noreply.github.com>
2022-03-30 18:58:40 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import argparse
 from train import *
-from model import AE_ENC
+from model import NE_WNA
 from utils import *
 from datasets import *
 import warnings
@@ -36,7 +36,7 @@ if args.dataset == "cora" or args.dataset == "citeseer" or args.dataset == "pubm
     if args.dataset == "pubmed":
         enhanced_adj = get_A_hat_k_power(sp_A_hat, args.order)
         print("Precompute Finish!")
-        model = NE(args.hidden, args.hidden_z, features.shape[1], labels.max().item() + 1, args.dropout)
+        model = NE_WNA(args.hidden, args.hidden_z, features.shape[1], labels.max().item() + 1, args.dropout)
         print(f'Dataset:{args.dataset}')
         run_citation(model, args.runs, args.epochs, args.lr, args.weight_decay, args.early_stopping, args.alpha,
                      args.beta,
@@ -46,7 +46,7 @@ if args.dataset == "cora" or args.dataset == "citeseer" or args.dataset == "pubm
     else:
         enhanced_adj = get_ehanced_A_hat_k_power(sp_A_hat, args.order)
         print("Precompute Finish!")
-        model = AE_ENC(args.hidden, args.hidden_z, features.shape[1], labels.max().item() + 1, args.dropout)
+        model = NE_WNA(args.hidden, args.hidden_z, features.shape[1], labels.max().item() + 1, args.dropout)
         print(f'Dataset:{args.dataset}')
         run_citation(model, args.runs, args.epochs, args.lr, args.weight_decay, args.early_stopping, args.alpha,
                      args.beta,
@@ -65,7 +65,7 @@ elif args.dataset == "computers" or args.dataset == "photo":
     print("Precompute Finish!")
     print(f'Dataset:{args.dataset}')
     print("Dataset:", dataset[0])
-    model = AE_ENC(args.hidden, args.hidden_z, dataset.num_features, dataset.num_classes, args.dropout)
+    model = NE_WNA(args.hidden, args.hidden_z, dataset.num_features, dataset.num_classes, args.dropout)
     run_coauthor_amazon(dataset, model, args.runs, args.epochs, args.lr, args.weight_decay, args.early_stopping,
                         args.alpha, args.beta, args.tau, enhanced_adj.to_dense().to(device), permute_masks, lcc=True)
 
@@ -77,6 +77,6 @@ elif args.dataset == "cs":
     sp_A_hat = sparse_mx_to_torch_sparse_tensor(A_hat).float()
     enhanced_adj = get_ehanced_A_hat_k_power(sp_A_hat, args.order)
     print("Dataset:", dataset[0])
-    model = AE_ENC(args.hidden, args.hidden_z, dataset.num_features, dataset.num_classes, args.dropout)
+    model = NE_WNA(args.hidden, args.hidden_z, dataset.num_features, dataset.num_classes, args.dropout)
     run_coauthor_amazon(dataset, model, args.runs, args.epochs, args.lr, args.weight_decay, args.early_stopping,
                         args.alpha, args.beta, args.tau, enhanced_adj.to_dense().to(device), permute_masks, lcc=False)
